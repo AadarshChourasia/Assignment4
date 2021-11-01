2305154a40ef284c1477188edfcd3926f415e509
AadarshChourasia <90022563+AadarshChourasia@users.noreply.github.com>
2021-11-01 18:00:47 +0530

    Create scope.js

---
+++ scope.js
@@ -0,0 +1,19 @@
+/**
+/ @author Aadarsh
+*/
+*
+var message = "in global";
+console.log("global: message + " + message);
+
+var a = function();{
+var message = "inside a";
+console.log("a : message = " + message);
+
+b();
+}
+
+function b(){
+	console.log("b: message = "+ message);
+
+}
+a();
